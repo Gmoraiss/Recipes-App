90a61ad065a5dbcc9bd5ebf873bde629598f9b6c
Petinger <lucaswinpetzinger@gmail.com>
2022-03-07 19:32:04 -0300

    requisito 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Login() {
-  const [inputEmail, setInputEmail] = useState();
-  const [inputPassword, setInputPassword] = useState();
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const MIN_LENGTH = 6;
+
+  useEffect(() => {
+    setBtnDisabled(!(inputEmail
+      .includes('@') && inputEmail
+      .includes('.com') && inputPassword.length > MIN_LENGTH));
+  }, [inputEmail, inputPassword]);
 
   const handleInputChange = ({ target }, setter) => {
     setter(target.value);
@@ -35,6 +43,7 @@ function Login() {
       </label>
 
       <button
+        disabled={ btnDisabled }
         data-testid="login-submit-btn"
         type="submit"
       >

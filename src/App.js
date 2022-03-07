a86c886c89d1e9abe2d7ade0f0aa1facff215323
Petinger <lucaswinpetzinger@gmail.com>
2022-03-07 16:19:48 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import RecipesProvider from './context/RecipesProvider';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <RecipesProvider>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/foods" component={ Foods } />
+        <Route exact path="/drinks" component={ Drinks } />
+        <Route exact path="/foods/:id" component={ RecipeDetails } />
+        <Route exact path="/drinks/:id" component={ RecipeDetails } />
+        <Route exact path="/foods/:id/in-progress" component={ ProgressRecipe } />
+        <Route exact path="/drinks/:id/in-progress" component={ ProgressRecipe } />
+        <Route exact path="/explore" component={ Explore } />
+        <Route exact path="/explore/foods" component={ ExploreRecipe } />
+        <Route exact path="/explore/drinks" component={ ExploreRecipe } />
+        <Route exact path="/explore/foods/ingredients" component={ Ingredients } />
+        <Route exact path="/explore/drinks/ingredients" component={ Ingredients } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
+        <Route exact path="/favorite-recipes" component={ Favorites } />
+      </RecipesProvider>
+    </Switch>
   );
 }
 
---
+++ src/context/RecipesProvider.jsx
@@ -0,0 +1,21 @@
+import React, { } from 'react';
+import PropTypes from 'prop-types';
+import MyContext from '.';
+
+function RecipesProvider({ children }) {
+  const store = {
+
+  };
+
+  return (
+    <MyContext.Provider value={ store }>
+      {children}
+    </MyContext.Provider>
+  );
+}
+
+RecipesProvider.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
+
+export default RecipesProvider;

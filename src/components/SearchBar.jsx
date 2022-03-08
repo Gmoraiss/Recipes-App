a21499e2b2cd14b6b1f64438ec7b8c4977bb8886
Pedro Goulart <ph.goulart.almeida@gmail.com>
2022-03-08 15:17:41 -0300

    requisito 15

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { fetchByIngredient, fetchByName, fetchFirstLetter } from '../servicesAPI';
 
 export default function SearchBar() {
   const [filterRadio, setFilterRadio] = useState('inputName');
   const [searchInput, setSearchInput] = useState('');
+  const [recipes, setRecipes] = useState([]);
+  const [isSearch, setisSearch] = useState(false);
 
-  const handleClick = (filter, text) => {
-    const filters = {
-      inputIngredient: fetchByIngredient,
-      inputName: fetchByName,
-      inputLetter: fetchFirstLetter,
-    };
-    filters[filter](text);
+  const history = useHistory();
+  const { location } = history;
+  const page = location.pathname.split('/')[1];
+
+  const filters = {
+    inputIngredient: fetchByIngredient,
+    inputName: fetchByName,
+    inputLetter: fetchFirstLetter,
+  };
+
+  const request2 = () => {
+    if (recipes.meals.length === 1) {
+      console.log('teste');
+      history.push(`/foods/${recipes.meals[0].idMeal}`);
+    }
+  };
+
+  const handleClick = async (filter, text) => {
+    filters[filter](text, page);
+    setisSearch(!isSearch);
     if (searchInput.length > 1 && filterRadio === 'inputLetter') {
       global.alert('Your search must have only 1 (one) character');
     }
   };
 
+  const request3 = async () => {
+    setRecipes(await filters[filterRadio](searchInput, page));
+    request2();
+  };
+
   return (
     <div>
       <div>
-
         <input
           type="text"
           placeholder="Search Recipe"
@@ -68,7 +88,10 @@ export default function SearchBar() {
         <button
           type="button"
           data-testid="exec-search-btn"
-          onClick={ () => handleClick(filterRadio, searchInput) }
+          onClick={ () => {
+            request3();
+            handleClick(filterRadio, searchInput);
+          } }
         >
           Search
 

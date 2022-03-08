b0b4fcaa6d383c26f7780dfb3ce4f224dfa878be
Petinger <lucaswinpetzinger@gmail.com>
2022-03-08 19:45:33 -0300

    requisitos 25 e 26

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,17 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Header from '../components/Header';
 import MyContext from '../context/index';
 import Footer from '../components/Footer';
+import { fetchDrinks } from '../servicesAPI';
 
 function Drinks() {
   const { recipes } = useContext(MyContext);
+  const [drinks, setDrinks] = useState([]);
+  const getDrinks = async () => {
+    setDrinks(await fetchDrinks());
+  };
 
+  useEffect(() => {
+    getDrinks();
+  }, []);
+
+  const render = recipes.length > 0 ? recipes : drinks;
   return (
     <div>
 
       <Header isSearchButton title="Drinks" />
 
-      {recipes.length > 0 && recipes.map((value, index) => (
+      {drinks.length > 0 && render.map((value, index) => (
         <div key={ index } data-testid={ `${index}-recipe-card` }>
           <img
             src={ value.strDrinkThumb }
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,16 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import MyContext from '../context/index';
+import { fetchMeal } from '../servicesAPI';
 
 function Foods() {
   const { recipes } = useContext(MyContext);
+  const [foods, setFoods] = useState([]);
 
+  const getFoods = async () => {
+    setFoods(await fetchMeal());
+  };
+
+  useEffect(() => {
+    getFoods();
+  }, []);
+
+  const render = recipes.length > 0 ? recipes : foods;
   return (
     <div>
       <Header isSearchButton title="Foods" />
 
-      {recipes.length > 0 && recipes.map((value, index) => (
+      {foods.length > 0 && render.map((value, index) => (
         <div key={ index } data-testid={ `${index}-recipe-card` }>
           <img
             src={ value.strMealThumb }

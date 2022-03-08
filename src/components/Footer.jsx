ecb8431118f1d1129a7eeeb7dd515ad544269ce9
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2022-03-08 15:57:29 -0300

    Requisito 20

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 function Footer() {
   return (
-    <footer data-testid="footer">
+    <footer
+      data-testid="footer"
+      className="fixed-bottom"
+    >
       <button
         data-testid="drinks-bottom-btn"
         type="button"
+        src={ drinkIcon }
       // onClick={ }
       >
         Bebidas
@@ -14,6 +21,7 @@ function Footer() {
       <button
         data-testid="explore-bottom-btn"
         type="button"
+        src={ exploreIcon }
       // onClick={  }
       >
         Explorar
@@ -22,6 +30,7 @@ function Footer() {
       <button
         data-testid="food-bottom-btn"
         type="button"
+        src={ mealIcon }
       // onClick={ }
       >
         Comida

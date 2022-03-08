ca8d729148b8548152e5a07b11459eb0fbeb4471
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2022-03-08 15:39:22 -0300

    Requisito 19

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,32 @@ import React from 'react';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer data-testid="footer">
+      <button
+        data-testid="drinks-bottom-btn"
+        type="button"
+      // onClick={ }
+      >
+        Bebidas
+      </button>
+
+      <button
+        data-testid="explore-bottom-btn"
+        type="button"
+      // onClick={  }
+      >
+        Explorar
+      </button>
+
+      <button
+        data-testid="food-bottom-btn"
+        type="button"
+      // onClick={ }
+      >
+        Comida
+      </button>
+
+    </footer>
   );
 }
 

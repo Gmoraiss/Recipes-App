f709c3c99eac9fb255d075f9179c25142cf93075
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2022-03-09 20:59:42 -0300

    guardando teste

--- src/pages/ExploreRecipe.jsx
+++ src/pages/ExploreRecipe.jsx
@@ -5,16 +5,67 @@ import Header from '../components/Header';
 
 function ExploreRecipe() {
   const history = useHistory();
-
   const pageTitle = history.location.pathname
     .split('/')[2];
   return (
     <div>
       <Header title={ pageTitle === 'foods' ? 'Explore Foods' : 'Explore Drinks' } />
-      <Footer />
+      <div>
 
+        <div>
+          <button
+            data-testid="explore-by-ingredient"
+            type="button"
+            className="explore-ingredient-btn"
+            onClick={ () => history.push('/explore/foods/ingredients') }
+          >
+            By Ingredient
+          </button>
+
+          <button
+            data-testid="explore-by-nationality"
+            type="button"
+            className="explore-nationality-btn"
+            onClick={ () => history.push('/explore/foods/nationalities') }
+          >
+            By Nationality
+          </button>
+
+          {/*
+          <button
+            data-testid="explore-surprise"
+            type="button"
+            className="explore-surprise-btn"
+            // onClick={ }
+            >
+            Surprise me!
+          </button> */}
+        </div>
+
+        {pageTitle === 'foods' && (
+          <div>
+            <button
+              data-testid="explore-by-ingredient"
+              type="button"
+              className="explore-ingredient-btn"
+              onClick={ () => history.push('/explore/drinks/ingredients') }
+            >
+              By Ingredient
+            </button>
+
+            <button
+              data-testid="explore-surprise"
+              type="button"
+              className="explore-surprise-btn"
+            // onClick={ }
+            >
+              Surprise me!
+            </button>
+          </div>
+        )}
+      </div>
+      <Footer />
     </div>
   );
 }
-
 export default ExploreRecipe;

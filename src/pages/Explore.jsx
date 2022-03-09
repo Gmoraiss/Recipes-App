aaa8f04a379567111043a4c7da1d2d4adb40c807
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2022-03-09 14:39:30 -0300

    Requisitos 67 ao 69

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Explore() {
+  const history = useHistory();
+
   return (
     <div>
       <Header title="Explore" />
-      <Footer />
+      <div>
+        <button
+          data-testid="explore-foods"
+          type="button"
+          className="explore-foods-btn"
+          onClick={ () => history.push('/explore/foods') }
+        >
+          Explore Foods
+        </button>
 
+        <button
+          data-testid="explore-drinks"
+          type="button"
+          className="explore-drinks-btn"
+          onClick={ () => history.push('/explore/drinks') }
+        >
+          Explore Drinks
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 }

e0694e14a5e1cd15a3a687bac12b39a36b2c17fe
Petinger <lucaswinpetzinger@gmail.com>
2022-03-11 10:13:31 -0300

    fix requisito 82 ao 87

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,8 +10,8 @@ function Profile() {
     history.push('/');
   };
 
-  const getEmail = JSON.parse(localStorage
-    .getItem('user')).email || 'você não está logado';
+  const getEmail = JSON.parse(localStorage.getItem('user'))
+    .email || 'você não está logado';
 
   return (
     <div>
